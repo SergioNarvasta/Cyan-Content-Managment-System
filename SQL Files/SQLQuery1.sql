cf039837ea906171203edb58550c5a7ab9c730df
Sergio Narvasta <sergio.jn024@gmail.com>
2022-06-03 18:27:24 -0500

    SQL File

---
+++ SQL Files/SQLQuery1.sql
@@ -0,0 +1,43 @@
+Use EQUILIBRA_V17
+
+Select  
+     a.FechaETA         ,      a.estado             ,      a.CodigoImportacion       ,       a.FechaIngAlm,
+	 a.OCompraSG        ,      a.TotalUSD           ,       C.ProveedorCEX       ,       B.ProductoCEX,
+	 a.CantidadTM       ,      a.CantidadCNT        ,       a.NroDAM             ,       a.FechaDAMLevante,
+	 a.FechaVB          ,      v.PAI_NOMCOR as Pais ,       a.NaveDestino        ,       a.BL,
+	 l.Naviera          ,      g.PuertoDestino         	
+	From CEX_Importacion A
+	Left Join Cex_ProductoCEX B on A.IdProductoCEX=B.IdProductoCEX
+	Left Join Cex_ProveedorCEX C on A.IdProveedorCEX=C.IdProveedorCEX
+	Left Join Cex_Seguimiento D on a.IdSeguimiento=d.IdSeguimiento
+	Left Join Cex_TipoCarga E on a.IdTipoCarga=e.IdTipoCarga
+	Left Join Cex_PuertoOrigen F on a.IdPuertoOrigen=f.IdPuertoOrigen
+	Left Join Cex_PuertoDestino G on a.IdPuertoDestino=G.IdPuertoDestino
+	Left Join CEX_Direccionamiento H on a.IdDireccionamiento = h.IdDireccionamiento
+	left join CEX_CanalCEX I on a.IdCanalCEX=i.IdCanalCex
+	left join CEX_Incoterm J on a.IdIncoterm=j.IdIncoterm
+	left join CEX_EmisionBL K on a.IdEmisionBL=k.IdEmisionBL
+	left join CEX_Naviera L on a.IdNaviera=l.IdNaviera
+	left join CEX_Regimen M on a.IdRegimen=m.IdRegimen
+	left join CEX_PresentacionCEX N on a.IdPresentacion=N.IdPresentacionCEX
+	left join CEX_PlazoPago O on a.IdPlazoPago=o.IdPlazoPago
+	left join CEX_DiasPlazo P on a.IdDiasPlazo=p.IdDiasPlazo
+	left join CEX_EstadoPago Q on a.IdEstadoPago=q.IdEstadoPago
+	Left Join CEX_TipoCarga02 R on a.IdTipoCarga02=r.IdTipoCarga02
+	left join PRODUCTOS_PRD prd on a.prd_codepk=prd.prd_codepk
+    Left Join CEX_ClasificacionMitsui S on b.IDClasificacionMitsui=s.IdClasificacionMitsui
+	Left Join CEX_Marca T on a.IdMarca=t.IdMarca
+	Left Join CEX_TerminoFletamiento U on a.IdFletamiento=U.IdFletamiento
+	Left Join pais_pai V on a.pai_codpai=v.PAI_CODPAI
+	Left Join CEX_TerminalMar W on a.IdTerminalMar=w.IdTerminalMar
+	Left Join CEX_AlmacenDestino X on a.IdAlmacenDestino=x.IdAlmacenDestino
+	left join( select cia_codcia,IdImportacion,
+				COUNT(IdImportacion) as CantFechaETD
+		  from CEX_ImportacionETD 
+		  group by cia_codcia,IdImportacion
+	)as etd on a.cia_codcia=etd.cia_codcia and a.IdImportacion=etd.IdImportacion
+	left join (select cia_codcia,IdImportacion,
+				COUNT(IdImportacion) as CantFechaETA
+		  from CEX_ImportacionETA 
+		  group by cia_codcia,IdImportacion
+	)as eta on a.cia_codcia=eta.cia_codcia and a.IdImportacion=eta.IdImportacion
