6af23a448e706e7031ae5bc3e548e44ad68ad0cf
Sergio Narvasta <sergio.jn024@gmail.com>
2022-06-07 18:07:50 -0500

    dev

--- Registro/Registro6/Registro6/Models/OrdenCompraImp.cs
+++ Registro/Registro6/Registro6/Models/OrdenCompraImp.cs
@@ -8,19 +8,19 @@ namespace Registro6.Models
         public int IdOrdenCompraImp { get; set; }
 
         [Display(Name = "TipoOrdenCompra")]
-        [Required(ErrorMessage = "Debe de ingresar el TipoOCompra")]
+        //[Required(ErrorMessage = "Debe de ingresar el TipoOCompra")]
         [MaxLength(13, ErrorMessage = "El campo no debe de tener mas de 13 caracteres")]
         public string TipoOCompra { get; set; }
 
         [Display(Name = "NroOrdenCompra")]
-        [Required(ErrorMessage = "Debe de ingresar NroOCompra")]
-        [MaxLength(12, ErrorMessage = "El campo no debe de tener mas de 12 caracteres")]
+        //[Required(ErrorMessage = "Debe de ingresar NroOCompra")]
+
         public int NroOCompra { get; set; }
 
         public DateTime Fecha { get; set; }
         public float TipoCambio { get; set; }
 
-        [Required(ErrorMessage = "Debe Seleccionar Estado")]
+       // [Required(ErrorMessage = "Debe Seleccionar Estado")]
         [MaxLength(12, ErrorMessage = "El campo no debe de tener mas de 12 caracteres")]
         public String Estado { get; set; }
 
@@ -28,20 +28,20 @@ namespace Registro6.Models
         public int CodInterno { get; set; }
 
         [Display(Name = "CotizacionVenta")]
-        [Required(ErrorMessage = "Debe de ingresar el CodCotizacion")]
-        [MaxLength(13, ErrorMessage = "El campo no debe de tener mas de 13 caracteres")]
+       // [Required(ErrorMessage = "Debe de ingresar el CodCotizacion")]
+        
         public int CodCotizacion { get; set; }
 
-        [Required(ErrorMessage = "Debe de ingresar Proveedor")]
-        [MaxLength(12, ErrorMessage = "El campo no debe de tener mas de 12 caracteres")]
+       // [Required(ErrorMessage = "Debe de ingresar Proveedor")]
+        [MaxLength(25, ErrorMessage = "El campo no debe de tener mas de 12 caracteres")]
         public String Proveedor { get; set; }
 
         [Required(ErrorMessage = "Debe de ingresar el Contacto")]
-        [MaxLength(15, ErrorMessage = "El campo no debe de tener mas de 15 caracteres")]
+        [MaxLength(25, ErrorMessage = "El campo no debe de tener mas de 15 caracteres")]
         public String Contacto { get; set; }
 
         [Required(ErrorMessage = "Debe de ingresar Area")]
-        [MaxLength(12, ErrorMessage = "El campo no debe de tener mas de 12 caracteres")]
+        [MaxLength(19, ErrorMessage = "El campo no debe de tener mas de 12 caracteres")]
         public String Area { get; set; }
 
         [Required(ErrorMessage = "Debe de ingresar Dato")]
