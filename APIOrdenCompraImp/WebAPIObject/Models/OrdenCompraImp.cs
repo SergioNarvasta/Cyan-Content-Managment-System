ab57c8990548c7cf437bff8c204e0d4924033c8c
Sergio Narvasta <sergio.jn024@gmail.com>
2022-06-07 16:25:32 -0500

    dev

--- APIOrdenCompraImp/WebAPIObject/Models/OrdenCompraImp.cs
+++ APIOrdenCompraImp/WebAPIObject/Models/OrdenCompraImp.cs
@@ -2,6 +2,24 @@
 {
     public class OrdenCompraImp
     {
+        [Key]
+        public int idOrdenCompraCab { get; set; }
 
+        [Display(Name = "RUC")]
+        [Required(ErrorMessage = "Debe de ingresar el RUC de la empresa")]
+        [MaxLength(10, ErrorMessage = "El campo no debe de tener mas de 10 caracteres")]
+        public string RUC { get; set; }
+
+        [Display(Name = "Razon Social")]
+        [Required(ErrorMessage = "Debe de ingrear la razon social del Proveedor")]
+        [MaxLength(80, ErrorMessage = "El campo no debe de tener mas de 80 caracteres")]
+        public string RazonSocial { get; set; }
+
+        [Display(Name = "Direccion")]
+        [Required(ErrorMessage = "Debe de ingresar la direccion del proveedor")]
+        [MaxLength(100, ErrorMessage = "El campo no debe de tener mas de 100 caracteres")]
+        public string Direccion { get; set; }
+
+        public virtual ICollection<OrdenCompraDet> OrdenCompraDet { get; set; }  //Para relacionar entidades
     }
 }
