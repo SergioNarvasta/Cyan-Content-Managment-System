8355e22c753423c342892a3d263e88a9c674de88
Sergio Narvasta <sergio.jn024@gmail.com>
2022-06-15 11:28:45 -0500

    dev

--- SQL Files/SQLQueryPivot.sql
+++ SQL Files/SQLQueryPivot.sql
@@ -5,8 +5,10 @@
 --group by a.CodigoImportacion,b.IdImportacion
 --group by b.FechaETD,b.NroSec
 
-
-Select a.CodigoImportacion, ETD.Fecha7,ETD.Fecha6,ETD.Fecha5,ETD.Fecha4,ETD.Fecha3,ETD.Fecha2,ETD.Fecha1
+Select a.CodigoImportacion,
+--COALESCE( 
+ETD.Fecha7,ETD.Fecha6,ETD.Fecha5,ETD.Fecha4,ETD.Fecha3,ETD.Fecha2,ETD.Fecha1 
+--)AS FECHA7_FECHA6_
 From CEX_Importacion A left join (
       Select b.cia_codcia,b.IdImportacion,
       (Case When b.NroSec=7 Then b.FechaETD End)AS Fecha7,
@@ -15,13 +17,36 @@ From CEX_Importacion A left join (
       (Case When b.NroSec=4 Then b.FechaETD End)AS Fecha4,
       (Case When b.NroSec=3 Then b.FechaETD End)AS Fecha3,
       (Case When b.NroSec=2 Then b.FechaETD End)AS Fecha2,
-      (Case When b.NroSec=1 Then b.FechaETD End)AS Fecha1 FROM CEX_ImportacionETD b )AS ETD 
+      (Case When b.NroSec=1 Then b.FechaETD End)AS Fecha1 FROM CEX_ImportacionETD b)AS ETD 
+   ON (a.cia_codcia=ETD.cia_codcia and a.IdImportacion=ETD.IdImportacion)
+group by a.CodigoImportacion,ETD.Fecha7,ETD.Fecha6,ETD.Fecha5,ETD.Fecha4,ETD.Fecha3,ETD.Fecha2,ETD.Fecha1
+
+GO
+
+
+
+Select a.CodigoImportacion,
+COALESCE( 
+ETD.Fecha7,ETD.Fecha6,ETD.Fecha5,ETD.Fecha4,ETD.Fecha3,ETD.Fecha2,ETD.Fecha1 
+)AS FECHA7_FECHA6_
+From CEX_Importacion A left join (
+      Select b.cia_codcia,b.IdImportacion,
+      (Case When b.NroSec=7 Then b.FechaETD End)AS Fecha7,
+      (Case When b.NroSec=6 Then b.FechaETD End)AS Fecha6,
+      (Case When b.NroSec=5 Then b.FechaETD End)AS Fecha5,
+      (Case When b.NroSec=4 Then b.FechaETD End)AS Fecha4,
+      (Case When b.NroSec=3 Then b.FechaETD End)AS Fecha3,
+      (Case When b.NroSec=2 Then b.FechaETD End)AS Fecha2,
+      (Case When b.NroSec=1 Then b.FechaETD End)AS Fecha1 FROM CEX_ImportacionETD b)AS ETD 
    ON (a.cia_codcia=ETD.cia_codcia and a.IdImportacion=ETD.IdImportacion)
 group by a.CodigoImportacion,ETD.Fecha7,ETD.Fecha6,ETD.Fecha5,ETD.Fecha4,ETD.Fecha3,ETD.Fecha2,ETD.Fecha1
 
 
 
 go
+ 
+ --SELECT * FROM  CEX_ImportacionETD 
+ GO
 --(SELECT cia_codcia,IdImportacion,count(NroSec)AS cant FROM CEX_ImportacionETD group by cia_codcia,IdImportacion)
 --left join AS cETD ON (a.cia_codcia=cETD.cia_codcia and a.IdImportacion=cETD.IdImportacion)
 
