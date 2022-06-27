6be9d87d52b18a29a1b992adfd50ebf1722b59d8
Sergio Narvasta <sergio.jn024@gmail.com>
2022-06-27 09:43:50 -0500

    dev

--- Registro/Registro6/Registro6/Controllers/OrdenComprasController.cs
+++ Registro/Registro6/Registro6/Controllers/OrdenComprasController.cs
@@ -50,8 +50,6 @@ namespace Registro6.Controllers
         }
 
         // POST: OrdenCompras/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("IdOrdenCompraImp,TipoOCompra,NroOCompra,Fecha,TipoCambio,Estado,CodInterno,CodCotizacion,Proveedor,Contacto,Area,TipoCompra,CondicionPago")] OrdenCompraImp ordenCompraImp)
@@ -72,7 +70,6 @@ namespace Registro6.Controllers
             {
                 return NotFound();
             }
-
             var ordenCompraImp = await _context.OrdenCompraImp.FindAsync(id);
             if (ordenCompraImp == null)
             {
