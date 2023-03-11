d898357e4aea2a028c18f05abc6168e3777f5fca
SergioNarvasta <sergio.jn024@gmail.com>
2023-03-11 13:41:15 -0500

    Se agrego el servicio IContentMainRepository a la App

--- Site/Site/Program.cs
+++ Site/Site/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddTransient<IContentMainRepository,  ContentMainRepository>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
