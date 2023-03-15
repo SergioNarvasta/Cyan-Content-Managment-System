8ea075a1b962d0c0a4d7763348021d07bdc90c91
SergioNarvasta <sergio.jn024@gmail.com>
2023-03-15 00:25:54 -0500

    MenuOption Entity

---
+++ CMS/CMS/CMS.Dominio/Entidades/MenuOption.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+
+namespace CMS.Dominio.Entidades
+{
+	public class MenuOption
+	{
+		public ObjectId MenuOption_Id { get; set; }
+		public string MenuOption_Nombre { get; set; }
+        public int MenuOption_Estado { get; set; }
+		public int MenuOption_Orden  { get; set; }
+		public string Company_Pk { get; set; }
+	}
+}
