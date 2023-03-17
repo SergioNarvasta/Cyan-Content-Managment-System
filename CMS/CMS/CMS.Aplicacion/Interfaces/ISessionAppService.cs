7a63def5b9e33432ed2eff7dbbfa2ba4424bc781
SergioNarvasta <sergio.jn024@gmail.com>
2023-03-16 22:10:48 -0500

    ISessionAppService

---
+++ CMS/CMS/CMS.Aplicacion/Interfaces/ISessionAppService.cs
@@ -0,0 +1,15 @@
+﻿using CMS.Dominio.Entidades;
+using ReactVentas.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Aplicacion.Interfaces
+{
+	public interface ISessionAppService
+	{
+		Task<User> Session(Dtosesion request);
+	}
+}
