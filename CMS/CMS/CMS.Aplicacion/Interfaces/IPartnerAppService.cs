413dd9f2d66f924685cd50635e0252b6e6029705
SergioNarvasta <sergio.jn024@gmail.com>
2023-03-31 19:54:33 -0500

    IPartnerAppService in API

---
+++ CMS/CMS/CMS.Aplicacion/Interfaces/IPartnerAppService.cs
@@ -0,0 +1,13 @@
+﻿using CMS.Dominio.Entidades;
+
+namespace CMS.Aplicacion.Interfaces
+{
+    public interface IPartnerAppService
+	{
+        Task Delete(string id);
+        Task<IEnumerable<Partner>> GetAll();
+		Task<Partner> GetById(string id);
+		Task Insert(Partner model);
+        Task Update(Partner model);
+    }
+}
---
+++ CMS/CMS/CMS.Dominio/Entidades/Partner.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using CMS.Dominio.Comunes;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CMS.Dominio.Entidades
+{
+    public class Partner : FileUnit
+    {
+        [BsonId]
+        public ObjectId Partner_Id { get; set; }
+
+        public string Partner_Pk { get; set; }
+
+        public string Partner_Nombre { get; set; }
+
+        public string Partner_Descripcion { get; set; }
+
+        public int Partner_Estado { get; set; }
+
+        public int Partner_Orden { get; set; }
+        public string Company_Pk { get; set; }
+    }
+}
