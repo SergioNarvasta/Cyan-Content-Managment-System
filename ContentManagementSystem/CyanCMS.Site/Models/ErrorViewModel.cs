88f0d2fa958b003689976457f68436bd4c5c8763
Sergio Narvasta <sergio.jn024@gmail.com>
2023-12-02 18:41:08 -0500

    Clean Project

--- ContentManagementSystem/CyanCMS.Site/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CyanCMS.Site.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
