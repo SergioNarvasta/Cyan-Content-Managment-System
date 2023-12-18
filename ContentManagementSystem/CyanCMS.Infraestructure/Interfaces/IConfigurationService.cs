36b15d4405dce48ddf0f8449a70640ada2024f2e
Sergio Narvasta <sergio.jn024@gmail.com>
2023-12-18 10:01:27 -0500

    Add IConfigurationService

---
+++ ContentManagementSystem/CyanCMS.Infraestructure/Interfaces/IConfigurationService.cs
@@ -0,0 +1,15 @@
+﻿using CMS.Dominio.Entidades;
+using CyanCMS.Domain.Entities;
+using CyanCMS.Utils.Request;
+
+namespace CyanCMS.Infraestructure.Interfaces
+{
+    public interface IConfigurationService
+    {
+        Task<List<Configuration>> GetByCompanyId(int companyId);
+        Task<Configuration> GetById(string id);
+        Task<int> GetCountByCompany(int companyId);
+        Task<bool> Insert(Configuration model);
+        Task<bool> Update(Configuration model);
+    }
+}
---
+++ ContentManagementSystem/CyanCMS.Infraestructure/Services/ConfigurationService.cs
@@ -0,0 +1,73 @@
+﻿
+using CyanCMS.Infraestructure.Data;
+using CyanCMS.Domain.Entities;
+using CyanCMS.Infraestructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace CyanCMS.Infraestructure.Services
+{
+    public class ConfigurationService : IConfigurationService
+    {
+        private readonly AppDbContext _dbContext;
+        public ConfigurationService(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Configuration>> GetByCompanyId(int companyId)
+        {
+            return await _dbContext
+                 .Configuration
+                 .Where(s => s.CompanyId== companyId)
+                 .AsNoTracking()
+                 .ToListAsync();               
+        }
+
+        public async Task<Configuration> GetById(string id)
+        {
+            var configurationEmpty = new Configuration();
+            return await _dbContext
+                .Configuration
+                .FindAsync(id) ?? configurationEmpty;             
+        }
+        public async Task<int> GetCountByCompany(int companyId)
+        {
+            var list = await _dbContext
+                 .Configuration
+                 .Where(s => s.CompanyId == companyId)
+                 .AsNoTracking()
+                 .ToListAsync();
+
+            return list.Count;
+        }
+        public async Task<bool> Insert(Configuration model)
+        {
+            try
+            {
+                _dbContext.Configuration.Add(model);
+                await _dbContext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            } 
+        }
+
+        public async Task<bool> Update(Configuration model)
+        {
+            try
+            {
+                _dbContext.Configuration.Update(model);
+                await _dbContext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }  
+        }
+    }
+}
